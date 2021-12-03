f0498c1670b5b33be960cff82d6da6e1aca76ccb
linda-xiao <lindaxiaos@gmail.com>
2021-12-03 23:59:23 +0100

    Completed day 3

---
+++ 2021/3/3_1.py
@@ -0,0 +1,51 @@
+# DAY 3: Task 1
+
+input = open('2021/3/input.txt').read().split('\n')
+
+# Remove the last (empty) element in the list
+input.pop()
+
+# List of gamma and epsilon rate
+gammaRate = []
+epsilonRate = []
+
+for bitPosition in range(0,len(input[0])):
+
+    # Start counter for zeros and ones
+    zeros = 0
+    ones = 0
+
+    for number in input:
+
+        # If the digit at the current position is 0, add to the zeros counter
+        if number[bitPosition] == '0':
+            zeros += 1
+
+        # If the digit at the current position is 1, add to the ones counter
+        else:
+            ones += 1
+
+    # If 0 is the most common bit
+    if zeros > ones:
+
+        # Append 0 to the gamma rate and 1 to the epsilon rate
+        gammaRate.append('0')
+        epsilonRate.append('1')
+
+    # If 1 is the most common bit
+    else:
+
+        # Append 1 to the gamma rate and 0 to the epsilon rate
+        gammaRate.append('1')
+        epsilonRate.append('0')
+
+# Convert from list to strings
+gammaBinary = ''.join(gammaRate)
+epsilonBinary = ''.join(epsilonRate)
+
+# Convert from binary to decimal
+gammaDec = int(gammaBinary,2)
+epsilonDec = int(epsilonBinary,2)
+
+# Power consumption (multiply gamma rate with epsilon rate)
+print(gammaDec * epsilonDec)
---
+++ 2021/3/3_2.py
@@ -0,0 +1,103 @@
+# DAY 3: Task 2
+
+input = open('2021/3/input.txt').read().split('\n')
+
+# Remove the last (empty) element in the list
+input.pop()
+
+# Creates an updated list for oxygen generator rating
+# with only the numbers containing the most common bit for each position
+def newListOxygen(input,bitPosition):
+
+    # List of new numbers
+    newNumbersOxygen = []
+
+    # Start counter for zeros and ones
+    zeros = 0
+    ones = 0
+
+    for number in input:
+
+        # If the digit at the current position is 0, add to the zeros counter
+        if number[bitPosition] == '0':
+            zeros += 1
+
+        # If the digit at the current position is 1, add to the ones counter
+        else:
+            ones += 1
+
+    for number in input:
+
+        # If 0 is the most common bit
+        if zeros > ones:
+
+            # Append all numbers with 0 at the current position
+            if number[bitPosition] == '0':
+                newNumbersOxygen.append(number)
+
+        # If 1 is the most common bit
+        else:
+
+            # Append all numbers with 1 at the current position
+            if number[bitPosition] == '1':
+                newNumbersOxygen.append(number)
+
+    # Return the updated list of numbers
+    return newNumbersOxygen
+
+# Creates an updated list for CO2 scrubber rating
+# with only the numbers containing the most common bit for each position
+def newListCO2(input,bitPosition):
+
+    newNumbersCO2 = []
+
+    zeros = 0
+    ones = 0
+
+    for number in input:
+
+        if number[bitPosition] == '0':
+            zeros += 1
+        else:
+            ones += 1
+
+    for number in input:
+        if zeros > ones:
+
+            if number[bitPosition] == '1':
+                newNumbersCO2.append(number)
+
+        else:
+            if number[bitPosition] == '0':
+                newNumbersCO2.append(number)
+
+    return newNumbersCO2
+
+# Initial input for oxygen generator rating and CO2 scrubber rating
+oxygen = input
+CO2 = input
+
+for bitPosition in range(0,len(input[0])):
+
+    # If the list contains more than one number
+    if len(oxygen) > 1:
+
+        # Update the list of numbers for oxygen generator rating
+        oxygen = newListOxygen(oxygen,bitPosition)
+
+    # If the list contains more than one number
+    if len(CO2) > 1:
+
+        # Update the list of numbers for CO2 scrubber rating
+        CO2 = newListCO2(CO2,bitPosition)
+
+# Convert from list to strings
+oxygenBinary = ''.join(oxygen)
+CO2Binary = ''.join(CO2)
+
+# Convert from binary to decimal
+oxygenDec = int(oxygenBinary,2)
+CO2Dec = int(CO2Binary,2)
+
+# Life support rating (multiply oxygen generator rating with CO2 scrubber rating)
+print(oxygenDec * CO2Dec)
